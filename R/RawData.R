83137b9cfbf1a3ca6f9f1de9e19647ef5ed28d36
Hania <hania.elkersh@student.uni-tuebingen.de>
2021-01-29 14:36:10 +0100

    added the raw data of X3 and X4

---
+++ R/RawData.R
@@ -0,0 +1,14 @@
+#' Data of the prior inference and utterance choice experiments
+#'
+#' This file contains the raw data from prior inference experiment.
+#'
+#' @docType data
+#'
+#' @usage data(priorInference)
+#'
+#' @keywords datasets
+#'
+#' @source \href{https://phenome.jax.org/projects/Moore1b}{QTL Archive}
+#'
+
+
